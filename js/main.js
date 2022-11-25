21fa9cd9c79e9cd751a7dfb2a4737a71586054af
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-25 15:55:49 +0100

    creato html, css, js di Hello World

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const {createApp} = Vue;
+
+    createApp({
+        data() {
+            return {
+                title : "Hello World",
+                myClass : "rosso",
+            }
+        }
+    }
+    ).mount('#myapp')

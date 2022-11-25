e36a236668863ce49a29a89f7b6acce992d9c1fe
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-25 17:08:01 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+
+
 const {createApp} = Vue;
 
     createApp({
         data() {
             return {
-                title : "Hello World",
+                title : "Hi Vue!!",
                 myClass : "rosso",
+                imgUrl : 'https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png'
             }
         }
     }
-    ).mount('#myapp')
+    ).mount('#myapp');
+
+
